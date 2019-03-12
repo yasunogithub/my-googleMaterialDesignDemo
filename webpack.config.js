ad30056e718f67bc4cc4b9ed62b0314a676099fd
SAKAMOTO Yasuaki <yasunoaddress.s@gmail.com>
2019-03-12 21:58:44 +0900

    Google Material Design Web．とりあえず上手くいったコード

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const glob = require('glob');
+const path = require('path');
+
+module.exports = [{
+  entry: './app.scss',
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+
+
+            loader: 'sass-loader',
+            options: {
+              includePaths: glob.sync('node_modules').map((d) => path.join(__dirname, d))
+            }
+          },
+          //            }{ loader: 'sass-loader' },
+        ]
+      }
+    ]
+  },
+}];
